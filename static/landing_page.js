8555145b0e9afcd0f58d1b14aaed34db42211f5e
hrrakesh <97217853+hrrakesh@users.noreply.github.com>
2024-03-05 22:01:29 +0530

    Add files via upload

---
+++ static/landing_page.js
@@ -0,0 +1,29 @@
+
+//--------popup---------------//
+
+document.getElementById("card-1-btn-1").addEventListener("click",function(){
+    document.querySelector(".popup").style.display ="flex";
+})
+document.querySelector(".fa-circle-xmark").addEventListener("click",function(){
+    document.querySelector(".popup").style.display ="none";
+})
+document.getElementById("card-1-btn-2").addEventListener("click",function(){
+    document.querySelector(".popup").style.display ="flex";
+})
+document.querySelector(".fa-circle-xmark").addEventListener("click",function(){
+    document.querySelector(".popup").style.display ="none";
+})
+document.getElementById("card-1-btn-3").addEventListener("click",function(){
+    document.querySelector(".popup").style.display ="flex";
+})
+document.querySelector(".fa-circle-xmark").addEventListener("click",function(){
+    document.querySelector(".popup").style.display ="none";
+})
+document.getElementById("card-1-btn-4").addEventListener("click",function(){
+    document.querySelector(".popup").style.display ="flex";
+})
+document.querySelector(".fa-circle-xmark").addEventListener("click",function(){
+    document.querySelector(".popup").style.display ="none";
+})
+
+
