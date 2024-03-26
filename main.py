662313e1729ad82ea19be814f45f749239f4fd05
hrrakesh <mandyarakesh23@gmail.com>
2024-03-26 21:17:34 +0530

    changed the name of python add home page function

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+# Landing page route
+@app.route('/')
+def landing_page():
+    return render_template('landing_page.html')
+
+# Login page route
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+# Signup page route
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
